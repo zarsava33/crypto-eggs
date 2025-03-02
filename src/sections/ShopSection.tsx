6c8de9837076393f4f17efa8bbff50a452133d97
Pavel Saveliev <mg_ajimas@hotmail.com>
2025-03-02 12:54:26 -0400

    fix: Corregir error de tipos en ShopSection

--- src/sections/ShopSection.tsx
+++ src/sections/ShopSection.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import { useGame } from '../contexts/GameContext';
 import { Shop } from '../components/Shop';
-import type { ShopItem } from '../types';
 
 export function ShopSection() {
-  const { state, actions } = useGame();
-
-  return (
-    <Shop
-      money={state.money}
-      onBuyEgg={actions.buyEgg}
-      onBuyBooster={actions.buyBooster}
-    />
-  );
+  return <Shop />;
 }
